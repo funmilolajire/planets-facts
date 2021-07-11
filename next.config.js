1a486158d2e35aa7bdcedb1bcb15dbb2005ff911
Funmilola O <funmilolajire@gmail.com>
2021-07-11 13:04:21 +0100

    setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/funmilolajire/image/upload/v1625999074/planets_facts'
+  }
 }
