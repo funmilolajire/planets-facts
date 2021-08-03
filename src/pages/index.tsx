f667756acf57a017292c9c50b90fb27171d63ca3
Funmilola O <funmilolajire@gmail.com>
2021-08-03 23:51:07 +0100

    turn off no-img-element rule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Meta from "../components/Meta/Meta";
 import Home from "../components/Home/Home"
 import { GetStaticProps } from "next";
 import axios from "axios";
+import absoluteUrl from 'next-absolute-url'
 import { useState, useEffect } from 'react';
 import { useAppContext } from '../contexts/state';
 
@@ -24,7 +25,8 @@ export default function Index({ planetsData }: { planetsData: PlanetData[] }) {
 
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await axios({ method: 'GET', url: process.env.PLANETS_URL })
+  const origin = typeof window !== 'undefined' && window.location.href ? window.location.href : '';
+  const response = await axios({ method: 'GET', url: `${origin + '/data.json'}` })
   const planetsData: PlanetData[] = response.data
   return {
     props: {
